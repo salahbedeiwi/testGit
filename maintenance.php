a0ca3203a9a085db9c9c1f30430f0835ea0404d0
Salah Bedeiwi <bedei001@umn.edu>
2019-05-13 07:09:16 -0500

    Added more info maintenance.php

--- maintenance.php
+++ maintenance.php
@@ -1,2 +1,4 @@
 <h1>maintenance.php</h1>
 <p>This page will show whenever there are some errors</p>
+<p>This page will show whenever there are some errors</p>
+<p>This page will show whenever there are some errors</p>
