d97fee55043ecb7a3a5234ce60a29f739056aad7
Salah Bedeiwi <bedei001@umn.edu>
2019-05-13 07:19:01 -0500

    Added few extra lines to maintance.php

--- maintenance.php
+++ maintenance.php
@@ -1,4 +1,5 @@
 <h1>maintenance.php</h1>
 <p>This page will show whenever there are some errors</p>
 <p>This page will show whenever there are some errors</p>
-<p>This page will show whenever there are some errors</p>
+<p>This page will show whenever there are some errors 123</p>
+<h2>Maintenance Page</h2>
