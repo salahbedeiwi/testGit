14b216de0e11237b0088338fe411d798b184ddfc
Salah Bedeiwi <bedei001@umn.edu>
2019-05-13 07:57:13 -0500

    Added few extra Heading tags into maintanance.php

--- maintenance.php
+++ maintenance.php
@@ -3,3 +3,5 @@
 <p>This page will show whenever there are some errors</p>
 <p>This page will show whenever there are some errors 123</p>
 <h2>Maintenance Page</h2>
+<h3>Maintenance Page</h3>
+<h4>Maintenance Page</h4>
