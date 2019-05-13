0e12e0c10699f21c073c3f20e483b68bab247e0a
Salah Bedeiwi <bedei001@umn.edu>
2019-05-13 07:20:09 -0500

    Added h2 to main.php

--- maintenance.php
+++ maintenance.php
@@ -2,3 +2,4 @@
 <p>This page will show whenever there are some errors</p>
 <p>This page will show whenever there are some errors</p>
 <p>This page will show whenever there are some errors</p>
+<h2>This line is for master</h2>
